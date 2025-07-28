d98496de3b0858f957004b88a20535e73c549f27
Josue1707 <josuemonro@gmail.com>
2025-07-27 21:02:08 -0600

    Add AsyncConfig class with @EnableAsync to support asynchronous method execution

---
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.tt._2025.b077.huellaspormexico.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
