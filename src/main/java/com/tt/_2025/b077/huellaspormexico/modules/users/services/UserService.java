03a5852531ff0597637141d24ebb5e771b192d43
Josue1707 <josuemonro@gmail.com>
2025-07-27 21:00:53 -0600

    Remove @Service annotation from UserService interface to prevent incorrect component scanning

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/users/services/UserService.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/users/services/UserService.java
@@ -2,13 +2,11 @@ package com.tt._2025.b077.huellaspormexico.modules.users.services;
 
 import com.tt._2025.b077.huellaspormexico.modules.users.entities.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
 
-@Service
 public interface UserService extends UserDetailsService {
     Optional<User> findByUsername(String username);
     User getUserProfile(String username);
