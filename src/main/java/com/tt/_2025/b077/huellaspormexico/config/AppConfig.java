a43ee67a06079ef68b5a426b3867b4d0ef3543de
Josue1707 <josuemonro@gmail.com>
2025-07-29 11:17:59 -0600

    Add AppConfig with RestTemplate bean and external property source

---
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.tt._2025.b077.huellaspormexico.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
