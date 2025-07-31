1a8dda889b71055c926e598667ddd565f1fcc4b9
Josue1707 <josuemonro@gmail.com>
2025-07-31 14:03:34 -0600

    feat: add weather controller with endpoints for coordinates and city weather data

---
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/controllers/weather/WeatherController.java
@@ -0,0 +1,45 @@
+package com.tt._2025.b077.huellaspormexico.modules.places.controllers.weather;
+
+import com.tt._2025.b077.huellaspormexico.models.ApiResponse;
+import com.tt._2025.b077.huellaspormexico.modules.places.dto.weather.WeatherSummary;
+import com.tt._2025.b077.huellaspormexico.modules.places.services.weather.WeatherService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @RequestMapping(path = "/coordinates", method = RequestMethod.GET)
+    public ResponseEntity<ApiResponse<?>> getWeatherByCoordinates(
+            @RequestParam Double lat,
+            @RequestParam Double lon) {
+
+        WeatherSummary weather = weatherService.getWeatherByCoordinates(lat, lon);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(HttpStatus.OK, "Clima obtenido correctamente", weather));
+    }
+
+    @RequestMapping(path = "/city", method = RequestMethod.GET)
+    public ResponseEntity<ApiResponse<?>> getWeatherByCity(
+            @RequestParam String name) {
+
+        WeatherSummary weather = weatherService.getWeatherByCity(name);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(HttpStatus.OK, "Clima obtenido correctamente", weather));
+    }
+}
