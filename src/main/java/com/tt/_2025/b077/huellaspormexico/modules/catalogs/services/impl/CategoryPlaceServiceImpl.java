ac44f41e67d4fe6980d8b737b9ac1c14ba7e6e95
Josue1707 <josuemonro@gmail.com>
2025-07-30 21:15:38 -0600

    feat: implement CategoryPlaceService with findAll functionality and sorted results

---
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/catalogs/services/impl/CategoryPlaceServiceImpl.java
@@ -0,0 +1,24 @@
+package com.tt._2025.b077.huellaspormexico.modules.catalogs.services.impl;
+
+import com.tt._2025.b077.huellaspormexico.modules.catalogs.entities.CategoryPlacesCatalog;
+import com.tt._2025.b077.huellaspormexico.modules.catalogs.repositories.CategoryPlacesRepository;
+import com.tt._2025.b077.huellaspormexico.modules.catalogs.services.CategoryPlaceService;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryPlaceServiceImpl implements CategoryPlaceService {
+
+    private final CategoryPlacesRepository categoryRepository;
+
+    public CategoryPlaceServiceImpl(CategoryPlacesRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<CategoryPlacesCatalog> findAll() {
+        return categoryRepository.findAll(Sort.by("id").ascending());
+    }
+}
