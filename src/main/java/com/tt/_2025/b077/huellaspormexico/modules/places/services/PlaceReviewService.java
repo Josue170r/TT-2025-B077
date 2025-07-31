3f9b3b9354978572179f977b1870a48519480146
Josue1707 <josuemonro@gmail.com>
2025-07-31 12:53:27 -0600

    feat: implement PlaceReviewService interface and service layer for place review operations

---
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/services/PlaceReviewService.java
@@ -0,0 +1,12 @@
+package com.tt._2025.b077.huellaspormexico.modules.places.services;
+
+import com.tt._2025.b077.huellaspormexico.modules.places.dto.PlaceReviewRequest;
+import com.tt._2025.b077.huellaspormexico.modules.places.entities.PlaceReview;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+
+public interface PlaceReviewService {
+    List<PlaceReview> findAllByPlaceId(Long placeId);
+    PlaceReview save(Long placeId, PlaceReviewRequest request, Authentication auth);
+}
