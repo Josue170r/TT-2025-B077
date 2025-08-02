88a0d4320700aa9e54976e23e08e6a2e598eb2b7
Josue1707 <josuemonro@gmail.com>
2025-08-02 13:59:12 -0600

    Add state catalog endpoints to CatalogController with CRUD operations

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/catalogs/controller/CatalogController.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/catalogs/controller/CatalogController.java
@@ -2,12 +2,12 @@ package com.tt._2025.b077.huellaspormexico.modules.catalogs.controller;
 
 import com.tt._2025.b077.huellaspormexico.models.ApiResponse;
 import com.tt._2025.b077.huellaspormexico.modules.catalogs.entities.CategoryPlacesCatalog;
+import com.tt._2025.b077.huellaspormexico.modules.catalogs.entities.StateCatalog;
 import com.tt._2025.b077.huellaspormexico.modules.catalogs.services.CategoryPlaceService;
+import com.tt._2025.b077.huellaspormexico.modules.catalogs.services.StateService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +16,11 @@ import java.util.List;
 public class CatalogController {
 
     private final CategoryPlaceService categoryPlaceService;
+    private final StateService stateService;
 
-    public CatalogController(CategoryPlaceService categoryPlaceService) {
+    public CatalogController(CategoryPlaceService categoryPlaceService, StateService stateService) {
         this.categoryPlaceService = categoryPlaceService;
+        this.stateService = stateService;
     }
 
     @RequestMapping(path = "/category-places", method = RequestMethod.GET)
@@ -29,4 +31,27 @@ public class CatalogController {
                 .body(ApiResponse.of(HttpStatus.OK, null, categoryPlaces));
     }
 
+    @RequestMapping(path = "/states", method = RequestMethod.GET)
+    public ResponseEntity<ApiResponse<?>> getStates() {
+        List<StateCatalog> states = stateService.findAll();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(HttpStatus.OK, null,states));
+    }
+
+    @RequestMapping(path = "/states/{id}", method = RequestMethod.GET)
+    public ResponseEntity<ApiResponse<?>> getStateById(@PathVariable("id") Long id) {
+        StateCatalog state = stateService.findById(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(HttpStatus.OK, null,state));
+    }
+
+    @RequestMapping(path = "/states/search", method = RequestMethod.GET)
+    public ResponseEntity<ApiResponse<?>> getStateByState(@RequestParam(value = "state") String state) {
+        StateCatalog stateFound = stateService.findByState(state);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.of(HttpStatus.OK, null,stateFound));
+    }
 }
