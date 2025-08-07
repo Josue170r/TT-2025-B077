552610d0160ccc184b692f47353d96b997e736c9
Josue1707 <josuemonro@gmail.com>
2025-08-06 21:57:04 -0600

    Add PlaceFavorite and PlaceHistory entities with user and place relationships

---
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/engagement/entities/PlaceHistory.java
@@ -0,0 +1,40 @@
+package com.tt._2025.b077.huellaspormexico.modules.engagement.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tt._2025.b077.huellaspormexico.modules.places.entities.Place;
+import com.tt._2025.b077.huellaspormexico.modules.users.entities.User;
+import com.tt._2025.b077.huellaspormexico.utils.BaseModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = false)
+@SuperBuilder
+@Data
+@Table(
+    name = "history",
+    uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"user_id", "place_id"})
+    }
+)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceHistory extends BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "place_id")
+    private Place place;
+}
