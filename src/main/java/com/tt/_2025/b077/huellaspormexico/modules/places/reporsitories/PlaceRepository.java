efe16a72101045ffe3b3e420d8d3bdf55ff9cb77
Josue1707 <josuemonro@gmail.com>
2025-08-21 14:00:45 -0600

    Add queries to fetch active places and filter by category in PlaceRepository

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/reporsitories/PlaceRepository.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/reporsitories/PlaceRepository.java
@@ -4,6 +4,8 @@ import com.tt._2025.b077.huellaspormexico.modules.places.entities.Place;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,15 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
     Optional<Place> findByPlaceId(String placeId);
     Page<Place> findByIdIn(List<Long> ids, Pageable pageable);
     List<Place> findAllByPlaceIdIn(List<String> placeIds);
+    List<Place> findAllByIsActiveTrue();
+
+    @Query("SELECT DISTINCT p FROM Place p " +
+            "JOIN p.placeTypes pt " +
+            "WHERE p.isActive = true " +
+            "AND pt.id IN (" +
+            "  SELECT cpt.id FROM CategoryPlacesCatalog c " +
+            "  JOIN c.placeTypes cpt " +
+            "  WHERE c.id = :categoryId AND c.isActive = true" +
+            ")")
+    List<Place> findByCategoryId(@Param("categoryId") Long categoryId);
 }
