2777c347087eb000951fd225725710019c8d0b13
Gerardo Ortiz <mruriel982@gmail.com>
2025-08-22 12:12:57 -0600

    Se gregan nuevas pantallas para pruebas (aun no tienen contenido)

---
+++ Frontend-Vue/front-tt-2025-b077/src/views/itinerary.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pb-16"> <!-- este padding evita que el contenido quede oculto -->
+    <h1 class="text-center text-2xl font-bold mt-4">Bienvenido</h1>
+    <p class="p-4">ITINERARIO</p>
+
+    <BottomNavbar />
+  </div>
+</template>
+
+<script>
+import BottomNavbar from "@/components/bottomnavbar.vue";
+
+export default {
+  components: { BottomNavbar }
+};
+</script>
