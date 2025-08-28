79166d423a85139ca6094486253cb5ab60b41dad
Josue1707 <josuemonro@gmail.com>
2025-08-28 11:20:41 -0600

    Refactor PlaceApiService to use FetchMode and return List<Place> instead of List<String>

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/services/PlaceApiService.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/services/PlaceApiService.java
@@ -2,10 +2,11 @@ package com.tt._2025.b077.huellaspormexico.modules.places.services;
 
 import com.tt._2025.b077.huellaspormexico.modules.places.dto.NearBySearchRequest;
 import com.tt._2025.b077.huellaspormexico.modules.places.entities.Place;
+import com.tt._2025.b077.huellaspormexico.modules.places.enums.FetchMode;
 
 import java.util.List;
 
 public interface PlaceApiService {
     Place fetchPlaceDetails(String placeId);
-    List<String> fetchNearBySearchPlaces(NearBySearchRequest request);
+    List<Place> fetchNearBySearchPlaces(NearBySearchRequest request);
 }
