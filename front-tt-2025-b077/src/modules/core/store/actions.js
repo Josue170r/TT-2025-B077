167419dd4119f5214ccabb06860b7ea927a4b402
Josue1707 <josuemonro@gmail.com>
2025-09-02 14:50:29 -0600

    Integrate home view with backend and use core store; update actions/getters/index/state, replace mutation.js with mutations.js, and set auth rule on home route

---
+++ front-tt-2025-b077/src/modules/core/store/actions.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+
+export async function fetchNearbyPlaces({ commit }, { latitude, longitude, types }) {
+  commit(
+      'setLoading',
+      {
+      isLoading: true,
+      msg: 'Buscando lugares cercanos',
+      },
+      { root: true }
+  )
+  return new Promise((resolve, reject) => {
+      axios
+      .post('/place/nearby-search', {
+          latitude,
+          longitude,
+          types
+      })
+      .then((response) => {
+        const data = response.data
+        commit('setPlaceIds', data.data)
+        resolve()
+      })
+      .catch((error) => {
+          reject(error)
+      })
+      .finally(() => {
+          commit('setLoading', false, { root: true })
+      })
+  })
+}
+
+export async function fetchPreferencePlaces({ commit }, { latitude, longitude }) {
+  commit(
+    'setLoading',
+    {
+      isLoading: true,
+      msg: 'Cargando lugares según tus preferencias',
+    },
+    { root: true }
+  )
+  return new Promise((resolve, reject) => {
+    axios
+      .post('/place/nearby-preferences', {
+        latitude,
+        longitude
+      })
+      .then((response) => {
+        const data = response.data
+        commit('setPlaceIds', data.data)
+        resolve()
+      })
+      .catch((error) => {
+        reject(error)
+      })
+      .finally(() => {
+        commit('setLoading', false, { root: true })
+      })
+  })
+}
+
+export async function fetchPlacesByIds({ commit }, { place_ids, page = 0, size = 10}) {
+  commit(
+    'setLoading',
+    {
+      isLoading: true,
+      msg: 'Cargando información de lugares',
+    },
+    { root: true }
+  )
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`/place/allById?page=${page}&size=${size}`, {
+        place_ids
+      })
+      .then((response) => {
+        const data = response.data
+        commit('setPlaces', data.content)
+        commit('setPagination', data)
+        resolve()
+      })
+      .catch((error) => {
+        reject(error)
+      })
+      .finally(() => {
+        commit('setLoading', false, { root: true })
+      })
+  })
+}
