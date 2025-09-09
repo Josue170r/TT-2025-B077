95e9e3986db8bbbefe6c50671bf0d5d178578a30
Josue1707 <josuemonro@gmail.com>
2025-09-08 23:17:42 -0600

    Refactor API: add FetchMode to fetchPlaceDetails and return placeId list for nearby search

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/services/PlaceApiService.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/places/services/PlaceApiService.java
@@ -9,7 +9,7 @@ import com.tt._2025.b077.huellaspormexico.modules.places.enums.FetchMode;
 import java.util.List;
 
 public interface PlaceApiService {
-    Place fetchPlaceDetails(String placeId);
-    List<Place> fetchNearBySearchPlaces(NearBySearchRequest request);
+    Place fetchPlaceDetails(String placeId, FetchMode mode);
+    List<String> fetchNearBySearchPlaces(NearBySearchRequest request);
     List<SearchByNameResponse> searchPlacesByName(SearchByNameRequest request);
 }
