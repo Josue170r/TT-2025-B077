ce1b7c9b076cf7e1f2b62ee04bb4bbf6d142b9b9
Josue1707 <josuemonro@gmail.com>
2025-09-24 21:48:46 -0600

    Add findBySettlementId method to CertificatedHotelRepository

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/hotels/repositories/CertificatedHotelRepository.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/hotels/repositories/CertificatedHotelRepository.java
@@ -3,5 +3,8 @@ package com.tt._2025.b077.huellaspormexico.modules.hotels.repositories;
 import com.tt._2025.b077.huellaspormexico.modules.hotels.entities.CertificatedHotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CertificatedHotelRepository extends JpaRepository<CertificatedHotel, Long> {
+    List<CertificatedHotel> findBySettlementId(Long settlementId);
 }
