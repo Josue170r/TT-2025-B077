436368232006c67c9705f0aea84ebed97548e046
Josue1707 <josuemonro@gmail.com>
2025-10-06 23:16:28 -0600

    Expand trips store: add hotels-related getters, pagination and filters; introduce mutations for certifications, settlements and hotels; refactor state to a factory with initial pagination/filter values and store dates as ISO strings

--- front-tt-2025-b077/src/modules/trips/store/state.js
+++ front-tt-2025-b077/src/modules/trips/store/state.js
@@ -1,10 +1,31 @@
-export default {
-  favoriteIds: [],
-  states: [],
-  newItinerary: {
-    tripTitle: '',
-    selectedState: '',
-    startDate: '',
-    endDate: '',
-  },
+export default function () {
+  return {
+    states: [],
+    certifications: [],
+    settlements: [],
+    hotels: [],
+    newItinerary: {
+      tripTitle: '',
+      selectedState: '',
+      startDate: '',
+      endDate: '',
+    },
+    pagination: {
+      pageNumber: 0,
+      pageSize: 10,
+      totalElements: 0,
+      totalPages: 0,
+      first: true,
+      last: true,
+      numberOfElements: 0,
+      empty: true,
+    },
+    filters: {
+      certifications: [],
+      settlement: null,
+      useLocation: false,
+      latitude: null,
+      longitude: null,
+    },
+  }
 }
