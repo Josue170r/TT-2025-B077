74ac50ed714fc3ad9fd32e0ef78675393641e611
Josue1707 <josuemonro@gmail.com>
2025-10-06 23:12:53 -0600

    Add hotels route to trips module and improve route consistency across trip views

--- front-tt-2025-b077/src/modules/trips/router/index.js
+++ front-tt-2025-b077/src/modules/trips/router/index.js
@@ -1,45 +1,54 @@
-import Favorites from "../views/favorites.vue";
-import Itinerary from "../views/itinerary.vue";
-import Mytrips from "../views/mytrips.vue";
-import Newtrip from "../views/newtrip.vue";
-
+import Favorites from '../views/favorites.vue'
+import Itinerary from '../views/itinerary.vue'
+import Mytrips from '../views/mytrips.vue'
+import Newtrip from '../views/newtrip.vue'
+import Hotels from '../../trips/views/hotelsview.vue'
 
 export default [
-    {
-        path: '/trips/favorites',
-        name: 'favorites',
-        meta: {
-            title: 'Favoritos',
-            rule: 'auth',
-        },
-        component: Favorites,
+  {
+    path: '/trips/favorites',
+    name: 'favorites',
+    meta: {
+      title: 'Favoritos',
+      rule: 'auth',
     },
-    {
-        path: '/trips/itinerary',
-        name: 'itinerary',
-        meta: {
-            title: 'Mis itinerarios',
-            rule: 'auth',
-        },
-        component: Itinerary,
+    component: Favorites,
+  },
+  {
+    path: '/trips/itinerary',
+    name: 'itinerary',
+    meta: {
+      title: 'Mis itinerarios',
+      rule: 'auth',
     },
+    component: Itinerary,
+  },
 
-    {
-        path: '/trips/my-trips',
-        name: 'my-trips',
-        meta: {
-            title: 'Mis viajes',
-            rule: 'auth',
-        },
-        component: Mytrips,
+  {
+    path: '/trips/my-trips',
+    name: 'my-trips',
+    meta: {
+      title: 'Mis viajes',
+      rule: 'auth',
     },
-    {
-        path: '/trips/newtrip',
-        name: 'new-trips',
-        meta: {
-            title: 'Nuevo viaje',
-            rule: 'auth',
-        },
-        component: Newtrip,
+    component: Mytrips,
+  },
+  {
+    path: '/trips/newtrip',
+    name: 'new-trips',
+    meta: {
+      title: 'Nuevo viaje',
+      rule: 'auth',
     },
+    component: Newtrip,
+  },
+  {
+    path: '/place/hotels',
+    name: 'hotels',
+    meta: {
+      title: 'Hoteles',
+      rule: 'auth',
+    },
+    component: Hotels,
+  },
 ]
