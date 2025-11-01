26620da08c7a121c80b82d98e2014a1d9a127ac0
Josue1707 <josuemonro@gmail.com>
2025-10-31 21:00:49 -0600

    Add method to find days by itinerary ID

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/itineraries/repositories/ItineraryDayRepository.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/itineraries/repositories/ItineraryDayRepository.java
@@ -3,8 +3,10 @@ package com.tt._2025.b077.huellaspormexico.modules.itineraries.repositories;
 import com.tt._2025.b077.huellaspormexico.modules.itineraries.entities.ItineraryDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItineraryDayRepository extends JpaRepository<ItineraryDay, Long> {
+    List<ItineraryDay> findByItineraryId(Long itineraryId);
     Optional<ItineraryDay> findByIdAndItineraryId(Long dayId, Long itineraryId);
 }
