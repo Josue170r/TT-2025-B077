20c9ef5baf736c4861b7c486d5997c5eb8d7bb44
Josue1707 <josuemonro@gmail.com>
2025-11-03 16:11:21 -0600

    Add method to get all favorite IDs for user

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/engagement/services/FavoritesService.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/engagement/services/FavoritesService.java
@@ -4,7 +4,10 @@ import com.tt._2025.b077.huellaspormexico.modules.engagement.entities.PlaceFavor
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface FavoritesService {
     void toggleFavorite(String username, Long placeId);
+    List<Long> getAllFavoritesIds(String username);
     Page<PlaceFavorite> getAllByUserId(String username, Pageable pageable);
 }
