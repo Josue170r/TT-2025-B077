3a852c4b4fe729a408a7741c6870bc520f174e4b
Josue1707 <josuemonro@gmail.com>
2025-11-03 16:11:12 -0600

    Add method to find all favorites by user ID

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/engagement/repositories/FavoritesRepository.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/engagement/repositories/FavoritesRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FavoritesRepository extends JpaRepository<PlaceFavorite, Long> {
+    List<PlaceFavorite> findAllByUserId(Long userId);
     Page<PlaceFavorite> findAllByUserId(Long userId, Pageable pageable);
     Optional<PlaceFavorite> findByUserIdAndPlaceId(Long userId, Long placeId);
 }
