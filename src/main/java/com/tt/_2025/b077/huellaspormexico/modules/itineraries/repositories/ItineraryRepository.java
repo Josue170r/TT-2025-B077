ea6fe234fee22fce773bdc4c30f45bcc8f1bd508
Josue1707 <josuemonro@gmail.com>
2025-11-05 19:56:29 -0600

    Change findByUserId to return Page instead of List

--- src/main/java/com/tt/_2025/b077/huellaspormexico/modules/itineraries/repositories/ItineraryRepository.java
+++ src/main/java/com/tt/_2025/b077/huellaspormexico/modules/itineraries/repositories/ItineraryRepository.java
@@ -1,10 +1,12 @@
 package com.tt._2025.b077.huellaspormexico.modules.itineraries.repositories;
 
 import com.tt._2025.b077.huellaspormexico.modules.itineraries.entities.Itinerary;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
-    List<Itinerary> findByUserId(Long userId);
+    Page<Itinerary> findByUserId(Long userId, Pageable pageable);
 }
